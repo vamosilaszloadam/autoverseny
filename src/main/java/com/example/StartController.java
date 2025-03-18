aa57641c09ca7c6638bc2a6a11896c72da3dfc47
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-03-18 11:09:21 +0100

    A startScene létrehozva

---
+++ src/main/java/com/example/StartController.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class StartController {
+
+    //TODO: a kérdőjel helyére kell valami
+    @FXML
+    private ComboBox<?> betCombo;
+
+    @FXML
+    private TableView<?> betTable;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    void onClickAddButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickRaceButton(ActionEvent event) {
+
+    }
+
+}
